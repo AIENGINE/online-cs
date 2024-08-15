b3966dfb1c098b270714f903921e2a5952fad274
ali <engalidanish@gmail.com>
2024-08-15 23:27:43 +0200

    backend config

--- next.config.js
+++ next.config.js
@@ -9,5 +9,10 @@ module.exports = {
         pathname: '**'
       }
     ]
-  }
+  },
+  env: {
+    BACKEND_URL: process.env.NODE_ENV === 'development'
+      ? 'http://localhost:8787' // Local Cloudflare Worker URL
+      : 'https://summer-flower-b680.engalidanish.workers.dev/', // Deployed Cloudflare Worker URL
+  },
 }
